d12c931dc14e13a86ff5f9d5052efdf02628eed4
Adeeb Shah <adeebshah4@gmail.com>
2023-09-07 17:12:50 +0530

    Add files via upload

--- src/components/HomeTable/HomeTable.jsx
+++ src/components/HomeTable/HomeTable.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 //props extracing example
-function HomeTable({name}) {
+function HomeTable({ products }) {
   return (
-    <div>{name}</div>
-  )
+    <div>
+      <ul>
+        {products.map((dataItem, index) => (
+          <li>{dataItem.title}</li>
+        ))}
+      </ul>
+      {/* // example - 1 */}
+      {products.map((dataItem, index) => (
+        <img src={dataItem.images[0]} alt="" srcset="" />
+      ))}
+
+      {/* //example 2 */}
+      {products.map((dataItem, index) => {
+        return dataItem.images.map((image, index) => (
+          <img src={image} alt="" srcset="" />
+        ));
+      })}
+    </div>
+  );
 }
 
-export default HomeTable
+export default HomeTable;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import HomeTable from '../../components/HomeTable/HomeTable'
 import './home.css'
-
+import Axios from 'axios'
 function Home() {
-  // Proping example
+  const [products, setProducts] = useState([])
+  
+  useEffect(() => {
+
+    //-- methond 1
+    // Axios.get('https://dummyjson.com/products').then((response) => {
+    //   console.log(response.data.products)
+    // }).catch((error) => console.log(error))
+
+    productsData()
+  }, [])
+
+  //method 2 -- GET ALL CALL
+  const productsData = async () => {
+    try {
+      const {data} = await Axios.get('https://dummyjson.com/products')
+      setProducts(data.products)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  
+
   return (
-    <div className='mainTable'><HomeTable name='Adeeb'/></div>
+    <div>
+      <HomeTable products={products} />
+    </div>
   )
 }
 
